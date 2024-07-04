735fce1e495d0e3000b62dde58846f9070f60d06
Patchara Al-umaree <patcharaalumaree@gmail.com>
2024-07-04 13:26:01 +0700

    FIX

--- script.js
+++ script.js
@@ -9,20 +9,7 @@ Email: Patcharaalumaree@gmail.com
 GitHub: https://github.com/MrPatchara`);
 }
 
-//download ของจริง tool1
 document.getElementById('downloadButton1').addEventListener('click', function() {
-    const fileName = 'Download_Program_py_to_exe.zip'; // ชื่อไฟล์ที่ต้องการดาวน์โหลด
-    const filePath = `https://github.com/MrPatchara/tool-website/tree/main/tool1`; // เส้นทางไปยังไฟล์ที่ต้องการดาวน์โหลด
-
-    // สร้างลิงก์ดาวน์โหลดชั่วคราว
-    const link = document.createElement('a');
-    link.href = filePath;
-    link.download = fileName;
-    
-    // เพิ่มลิงก์ลงในเอกสารแล้วคลิกเพื่อเริ่มดาวน์โหลด
-    document.body.appendChild(link);
-    link.click();
-    
-    // ลบลิงก์หลังจากดาวน์โหลดเสร็จสิ้น
-    document.body.removeChild(link);
+    window.location.href = 'https://drive.google.com/file/d/1CoayGA76bFlM61vAxQnfNE5y5b2t97Le/view?usp=sharing';
 });
+
