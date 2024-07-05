b4638d29c9d47745343446f8fb15d42ad1bc792e
Patchara Al-umaree <patcharaalumaree@gmail.com>
2024-07-05 15:28:45 +0700

    update

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ document.getElementById('downloadButton1').addEventListener('click', function() 
     window.location.href = 'https://drive.google.com/file/d/1CoayGA76bFlM61vAxQnfNE5y5b2t97Le/view?usp=sharing';
 });
 
+document.getElementById('downloadButton2').addEventListener('click', function() {
+    window.location.href = 'https://drive.google.com/file/d/1NDhSm3LZudVJUzZ1PXVkJUiEHHvd1pg8/view?usp=sharing';
+});
+
