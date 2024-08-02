815a01b12a613c80cc70bbbbdf7ea3756641c919
Patchara Al-umaree <patcharaalumaree@gmail.com>
2024-08-02 14:48:47 +0700

    up

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ document.getElementById('downloadButton2').addEventListener('click', function() 
     window.location.href = 'https://drive.google.com/file/d/1NDhSm3LZudVJUzZ1PXVkJUiEHHvd1pg8/view?usp=sharing';
 });
 
+document.getElementById('downloadButton3').addEventListener('click', function() {
+    window.location.href = 'https://drive.google.com/file/d/1XX4Fd9wmzuN7H-C4cWKH9F645RVOdgmD/view?usp=drive_link';
+});
+
+document.getElementById('downloadButton4').addEventListener('click', function() {
+    window.location.href = 'https://drive.google.com/file/d/1gRksKFha6naMB15Hs3PN8VPpsdMr-iUq/view?usp=sharing';
+});
+
+
